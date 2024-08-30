7089c39262a3908e1f06d82c44a88e48bd6865fc
simbahebinbo <lansheng228@163.com>
2024-08-30 17:46:03 +0800

    upgrade conan to 2.x.x and upgrade boost to 1.86.0

--- full-build.sh
+++ full-build.sh
@@ -3,7 +3,7 @@
 rm -rf build
 mkdir build
 cd build
-conan install ..
+conan install .. --build=missing
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./Release/generators/conan_toolchain.cmake ..
-make
+cmake --build . --config Release
 cd ..
